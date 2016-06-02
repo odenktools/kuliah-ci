9c5e4c637e506f8609c3a10cec3025e2c4341797
rahyan <rahyan.ramadhani@gmail.com>
2016-06-02 18:31:32 +0700

    Add Config Files

---
+++ application/config/blade.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Config for the Redis library
+ *
+ * @see ../libraries/Redis.php
+ */
+
+// Connection details
+$config['views_path'] = APPPATH . 'views/blade/';
+$config['cache_path'] = APPPATH . 'cache/blade/';
